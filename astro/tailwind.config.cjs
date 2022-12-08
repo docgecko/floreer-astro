78b83935ceb932765488c057b81b8f03d24412c9
Daren Warburton <daren.sdw@gmail.com>
2022-12-08 18:29:12 +0000

    Footer social links

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				twitter: '#1DA1F2',
+				facebook: '#4267B2',
+				instagram: '#E1306C',
+				linkedin: '#0077B5',
+			},
+		},
 	},
 	plugins: [],
 }
