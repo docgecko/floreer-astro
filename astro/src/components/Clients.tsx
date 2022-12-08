0d3081b9ca0fdc0ef2f45074a3664d4c23524de2
Daren Warburton <daren.sdw@gmail.com>
2022-12-08 16:12:37 +0000

    Home page client's slides working

--- astro/src/components/Clients.tsx
+++ astro/src/components/Clients.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import type { Client } from '@lib/sanityApi';
 import { getClients } from '@lib/sanityApi';
+import ClientsSlide from "./ClientsSlide";
 
 export default function Clients(
     { slides, slideMin, slideMax }:
@@ -10,20 +11,13 @@ export default function Clients(
   const [hoverNext, setHoverNext] = useState<boolean>(false);
   const [hoverPrev, setHoverPrev] = useState<boolean>(false);
 
-  function updateSelectedSlide(direction: number): void {
-    console.log({ selectedSlide, slideMax, slideMin, direction})
-    if (direction === 1) {
-      if (selectedSlide < slideMax) {
-        setSelectedSlide(selectedSlide + 1);
-      } else {
-        setSelectedSlide(slideMin);
-      }
-    } else {
-      if (selectedSlide > slideMin) {
-        setSelectedSlide(selectedSlide - 1);
-      } else {
-        setSelectedSlide(slideMax);
-      }
+  function updateSelectedSlide(slideNo: number): void {
+    if (slideNo >= slideMin && slideNo <= slideMax) {
+      setSelectedSlide(slideNo);
+    } else if (slideNo < slideMin) {
+      setSelectedSlide(slideMax);
+    } else if (slideNo > slideMax) {
+      setSelectedSlide(slideMin);
     }
   }
   console.log({ selectedSlide })
@@ -36,28 +30,60 @@ export default function Clients(
           className="flex flex-wrap w-full h-72 transition duration-200 ease-in-out"
         >
           {slides.length > 0 && slides[selectedSlide].map((client: Client) => (
-            <a
-              className="flex justify-center items-center pt-4 pb-4 m-0 cursor-pointer hover:bg-gray-200 w-1/4 h-36"
-              key={client._id}
-            >
-              <div className="flex bg-transparent">
-                <img
-                  className="h-auto max-h-36 max-w-[148px]"
-                  src={client.image.url}
-                  alt={client.name}
-
-                />
-              </div>
-            </a>
+            <ClientsSlide client={client} key={client._id} />
           ))}
         </div>
       </div>
 
+      {/* Back Button */}
+      <div className="flex absolute top-0 left-0 w-12 h-full">
+        <a
+          className="flex relative w-12 h-full z-50 justify-center items-center cursor-pointer"
+          onClick={() => updateSelectedSlide(selectedSlide - 1)}
+          onMouseEnter={() => setHoverPrev(true)}
+          onMouseLeave={() => setHoverPrev(false)}
+        >
+          {hoverPrev === false ? (
+            <div
+              className="flex font-light justify-center items-center text-sm text-white bg-gray-500 rounded-full w-8 h-8"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          ) : (
+            <div
+              className="flex font-light justify-center items-center text-sm text-white bg-indigo-800 rounded-full w-8 h-8"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          )}
+        </a>
+      </div>
+
       {/* Forward Button */}
-      <div className="flex absolute top-0 -right-12 w-12 h-full">
+      <div className="flex absolute top-0 right-0 w-12 h-full">
         <div
           className="flex relative w-12 h-full z-50 justify-center items-center cursor-pointer"
-          onClick={() => updateSelectedSlide(1)}
+          onClick={() => updateSelectedSlide(selectedSlide + 1)}
           onMouseEnter={() => setHoverNext(true)}
           onMouseLeave={() => setHoverNext(false)}
         >
@@ -95,6 +121,16 @@ export default function Clients(
             </div>
           )}
         </div>
+      </div>
+
+      {/* Row of Buttons */}
+      <div className="flex flex-row relative w-full h-12 justify-center items-end">
+        {slides.length > 0 && slides.map((slide: Client[], index: number) => (
+          <a
+            className={`flex font-light justify-center items-center text-xs ${selectedSlide === index ? "bg-indigo-800" : "bg-gray-300 hover:bg-gray-500"} rounded-full w-3 h-3 mx-2 cursor-pointer`}
+            onClick={() => updateSelectedSlide(index)}
+          />
+        ))}
       </div>
     </>
   );
--- astro/src/components/ClientsSlide.tsx
+++ astro/src/components/ClientsSlide.tsx
@@ -1,27 +1,19 @@
 import type { Client } from '@lib/sanityApi';
 
-export default function ClientsSlide({slide}: any) {
-  console.log("slide data", slide);
-
+export default function ClientsSlide({client}: {client: Client}) {
   return (
-    <div className="flex flex-col relative w-full">
-      {slide && slide.map((client: Client) => (
-        <div
-          className="flex flex-wrap w-full h-72 transition duration-200 ease-in-out"
-        >
-          <a
-            className="flex justify-center items-center pt-4 pb-4 m-0 cursor-pointer hover:bg-gray-200 w-1/4 h-36"
-          >
-            <div className="flex bg-transparent">
-              <img
-                className="h-auto max-h-36 max-w-[148px]"
-                src={client.image.url}
-                alt={client.name}
-              />
-            </div>
-          </a>
-        </div>
-      ))}
-    </div>
+    <a
+      className="flex justify-center items-center pt-4 pb-4 m-0 cursor-pointer hover:bg-gray-200 w-1/4 h-36"
+      key={client._id}
+      href={`/work/case-studies?client=${client._id}`}
+    >
+      <div className="flex bg-transparent">
+        <img
+          className="h-auto max-h-36 max-w-[148px]"
+          src={client.image.url}
+          alt={client.name}
+        />
+      </div>
+    </a>
   );
 }
